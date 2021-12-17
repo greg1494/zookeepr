1a3210dc32c4fac6d50c72b45e55687f2327c75c
Greg Christiansen <gregchristiansen14@gmail.com>
2021-12-16 18:37:00 -0600

    fixed a bug

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
